382198627625d2fb47e943201eeca55a3e352b21
zhanglijun <29020076@qq.com>
2017-10-11 09:28:32 +0800

    add file

---
+++ src/main/java/tomcat/request/session/data/cache/impl/RedisConstants.java
@@ -0,0 +1,48 @@
+package tomcat.request.session.data.cache.impl;
+
+/**
+ * Tomcat clustering with Redis data-cache implementation.
+ * 
+ * Redis data-cache constants.
+ * 
+ * @author Ranjith Manickam
+ * @since 2.0
+ */
+public class RedisConstants {
+
+	// redis properties file name
+	public static final String PROPERTIES_FILE = "redis-data-cache.properties";
+
+	// redis properties
+	public static final String HOSTS = "redis.hosts";
+	public static final String CLUSTER_ENABLED = "redis.cluster.enabled";
+
+	public static final String MAX_ACTIVE = "redis.max.active";
+	public static final String TEST_ONBORROW = "redis.test.onBorrow";
+	public static final String TEST_ONRETURN = "redis.test.onReturn";
+	public static final String MAX_IDLE = "redis.max.idle";
+	public static final String MIN_IDLE = "redis.min.idle";
+	public static final String TEST_WHILEIDLE = "redis.test.whileIdle";
+	public static final String TEST_NUMPEREVICTION = "redis.test.numPerEviction";
+	public static final String TIME_BETWEENEVICTION = "redis.time.betweenEviction";
+
+	public static final String PASSWORD = "redis.password";
+	public static final String DATABASE = "redis.database";
+	public static final String TIMEOUT = "redis.timeout";			//链接超时
+	public static final String SO_TIMEOUT = "redis.sotimeout";		//服务请求超时
+	public static final String MAX_ATTEMPTS = "redis.maxattempt";	//请求重试次数
+	
+	// redis property default values
+	public static final String DEFAULT_MAX_ACTIVE_VALUE = "10";
+	public static final String DEFAULT_TEST_ONBORROW_VALUE = "true";
+	public static final String DEFAULT_TEST_ONRETURN_VALUE = "true";
+	public static final String DEFAULT_MAX_IDLE_VALUE = "5";
+	public static final String DEFAULT_MIN_IDLE_VALUE = "1";
+	public static final String DEFAULT_TEST_WHILEIDLE_VALUE = "true";
+	public static final String DEFAULT_TEST_NUMPEREVICTION_VALUE = "10";
+	public static final String DEFAULT_TIME_BETWEENEVICTION_VALUE = "60000";
+	public static final String DEFAULT_CLUSTER_ENABLED = "false";
+	public static final int    DEFAULT_MAX_ATTEMPTS = 5;				//最大失败重试次数
+
+	public static final String CONN_FAILED_RETRY_MSG = "Jedis connection failed, retrying...";
+}
---
+++ src/test/java/tomcat/request/session/redis/RedisConnectionTest.java
@@ -0,0 +1,20 @@
+package tomcat.request.session.redis;
+
+import org.junit.Test;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+public class RedisConnectionTest {
+	
+	@Test
+	public void testConnection(){
+		/*HostAndPort jedisClusterNode = ;
+		int connectionTimeout = 2000;
+		int soTimeout = 2000;
+		int maxAttempts = 5;
+		
+		new JedisCluster(jedisClusterNode , connectionTimeout, soTimeout, maxAttempts, poolConfig);*/
+	}
+
+}
